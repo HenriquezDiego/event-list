1773485176386588b1773ea39ed25867646accf2
Diego Henriquez <df.henriquez@hotmail.com>
2022-07-05 16:20:47 -0600

    hot fix using named routes

--- src/router/index.js
+++ src/router/index.js
@@ -7,21 +7,12 @@ const routes = [
   {
     path: "/",
     name: "home",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/HomeView.vue"),
+    component: () => import("../views/HomeView.vue"),
   },
   {
     path: "/about-us",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
-  {
-    path: "/about",
-    redirect: { name: "about" },
+    component: () => import("../views/AboutView.vue"),
   },
 ];
 
