30ba967c8252d4700e2dfcb4416d4921ca7387f3
Diego Henriquez <df.henriquez@hotmail.com>
2022-07-05 16:06:41 -0600

    router redirect.

--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,17 @@ const routes = [
       import(/* webpackChunkName: "about" */ "../views/HomeView.vue"),
   },
   {
-    path: "/about",
+    path: "/about-us",
     name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/about",
+    redirect: { name: "about" },
   },
 ];
 
