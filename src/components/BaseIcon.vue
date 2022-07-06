6ce6fd71ca7e48d452bf8898e374f1a9085b5462
Diego Henriquez <df.henriquez@hotmail.com>
2022-07-06 16:22:15 -0600

    automatic global registration.

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Base Icon</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,13 +2,16 @@
   <div>
     <h1>Events Listing</h1>
     <EventCard />
+    <BaseIcon />
   </div>
 </template>
 <script>
 import EventCard from "@/components/EnventCard.vue";
+import BaseIcon from "@/components/BaseIcon.vue";
 export default {
   components: {
     EventCard,
+    BaseIcon,
   },
 };
 </script>
