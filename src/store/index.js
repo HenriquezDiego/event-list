612f2bafcf13a5353a0f256160f05d042d168d0d
Diego Henriquez <df.henriquez@hotmail.com>
2022-07-08 16:48:44 -0600

    mutations and actions in CreateEnvent (I).

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,35 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import EventServcies from "@/services/EventService.js";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: "Diego Henriquez",
+    categories: [
+      "sustainability",
+      "nature",
+      "animal welfare",
+      "housing",
+      "education",
+      "food",
+      "community",
+    ],
+    events: [],
+  },
   getters: {},
-  mutations: {},
-  actions: {},
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event);
+    },
+  },
+  actions: {
+    createEvent({ commit }, event) {
+      return EventServcies.postEvent(event).then(() => {
+        commit("ADD_EVENT", event);
+      });
+    },
+  },
   modules: {},
 });
